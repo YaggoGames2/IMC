7ef003f7624399be39caba9d5c195a84b3b6fad1
YAGGO ROSA DE ALMEIDA SILVA <yaggo.rasilva@senacsp.edu.br>
2025-06-02 14:48:17 -0300

    --

--- js/Dom.js
+++ js/Dom.js
@@ -12,56 +12,56 @@ function calcular() {
     if (resultado < 16) {
  
         TipoDePeso = " Magreza Grau III"
-        Imagem = "/img/bellsprout.gif"
+        Imagem = "./img/bellsprout.gif"
         Pesoresultado.style.backgroundColor = "green";
         Pesoresultado.style.color = "yellow";
  
     } else if (resultado < 17) {
  
         TipoDePeso = " Magreza Grau II"
-        Imagem = "/img/exeguttordealola.gif"
+        Imagem = "./img/exeguttordealola.gif"
           Pesoresultado.style.backgroundColor = "beige";
         Pesoresultado.style.color = "green";
  
     } else if (resultado < 18.5) {
  
         TipoDePeso = " Magreza Grau I"
-        Imagem = "/img/pheoromosa.gif"
+        Imagem = "./img/pheoromosa.gif"
         Pesoresultado.style.backgroundColor = "white";
         Pesoresultado.style.color = "blue";
  
     } else if (resultado < 25) {
  
         TipoDePeso = " Eutrofia";
-        Imagem = "/img/Pausar GIF Pikachu GIFs - Get the best gif on GIFER.gif"
+        Imagem = "./img/Pausar GIF Pikachu GIFs - Get the best gif on GIFER.gif"
         Pesoresultado.style.backgroundColor = "red";
         Pesoresultado.style.color = "yellow";
  
     } else if (resultado < 30) {
  
         TipoDePeso = " Pré obesidade";
-        Imagem = "/img/pokemon-dragonite.gif"
+        Imagem = "./img/pokemon-dragonite.gif"
         Pesoresultado.style.backgroundColor = "orange";
         Pesoresultado.style.color = "blue";
  
     } else if (resultado < 35) {
  
         TipoDePeso = " Obesidade moderada Grau I";
-        Imagem = "/img/pokemon-snorlax.gif"
+        Imagem = "./img/pokemon-snorlax.gif"
         Pesoresultado.style.backgroundColor = "blue";
         Pesoresultado.style.color = "white";
  
     } else if (resultado < 40) {
  
         TipoDePeso = " Obesidade severa Grau II";
-        Imagem = "/img/slaking.gif";
+        Imagem = "./img/slaking.gif";
         Pesoresultado.style.backgroundColor = "beige";
         Pesoresultado.style.color = "black";
  
     } else {
  
         TipoDePeso = " Obesidade muito severa Grau III";
-        Imagem = "/img/PikachuGordao.gif";
+        Imagem = "./img/PikachuGordao.gif";
         Pesoresultado.style.backgroundColor = "red";
         Pesoresultado.style.color = "pink";
     }
